202b188602bc0596cb212b8d41b939591cc81254
sahilpatel18 <sahil.41801@gmail.com>
2023-01-06 17:25:09 -0500

    reusable card component created with bulma

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import ProfileCard from './ProfileCard'
-import AlexaImage from './images/alexa.png'
-import CortanaImage from './images/cortana.png'
-import SiriImage from './images/siri.png'
-
-console.log(AlexaImage)
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 const App = () => {
-    return (
-        <div>
-            <div>Personal Digital
-                <ProfileCard image={AlexaImage} title="Alexa" handle="@alexa123" />
-                <ProfileCard image={CortanaImage} title="Cortana" handle="@cortana123" />
-                <ProfileCard image={SiriImage} title="Siri" handle="@siri123" />
-                
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>Personal Digital Assistants</div>
 
-export default App
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa123"
+                image={AlexaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana123"
+                image={CortanaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri123" image={SiriImage} />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,14 +1,20 @@
+const ProfileCard = ({ title, handle, image }) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda" />
+        </figure>
 
-
-const ProfileCard = ({title, handle, image}) => {
-    return (
-    <div>
-        <img src={image} />
-        <div>Title:{title} </div>
-        <div>Handle:{handle} </div>
-
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default ProfileCard;
